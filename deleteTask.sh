c55be05bcb80890b6459dfa75cfc3881db57e63a
Mauricio ML <mau_ml@comunidad.unam.mx>
2022-07-20 21:26:35 +0000

    feature(flags): Adding flags to do more actions

--- deleteTask.sh
+++ deleteTask.sh
@@ -1,5 +1,29 @@
 #!bin/bash
-taskIds=$(todoist l | awk '{print $1}')
-firstId=$(echo $taskIds | awk '{print $1}')
+
+while getopts d:c:m:s: flag; do
+	case $flag in
+		d)	
+			echo "Deleting $OPTARG..."
+				task=$(todoist l | grep $OPTARG)	
+				id=$(echo $task | awk '{print $1}')
+				if ! $(todoist d $id); then
+					echo "The $OPTARG task doesn't exist"
+				else
+					echo "$OPTARG has been deleted."
+				fi
+			;;
+		c) 
+			echo "closing $OPTARG"
+			;;
+		m)
+			echo "modifying $OPTARG"
+			;;
+		s)
+			echo "showing $OPTARG"
+			;;
+	esac
+done
+#taskIds=$(todoist l | awk '{print $1}')
+#firstId=$(echo $taskIds | awk '{print $1}')
 #echo $firstId
-todoist d $firstId
+#todoist d $firstId
